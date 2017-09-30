54396a7823a20f6660bb40d26b345d3e514e8321
maelon <maelon.j@gmail.com>
2017-09-30 19:10:43 +0800

    update

--- src/element/index.js
+++ src/element/index.js
@@ -4,10 +4,36 @@
 #       Email: maelon.j@gmail.com
 #  CreateTime: 2017-09-29 11:21
 # Description: The base unit of jCircuit.
-               It's composed of input, output and process, like a electrical element.
-               One element only contains one input, and one output, and one process.
-               The input starts process.
-               The output is called by process when it is done.
 ===================================================================*/
 
 'use strict';
+
+/**
+ * The base unit of a circuit.
+ * It's composed of input, output and process, like a electrical element.
+ * One element only contains one input, and one output, and one process.
+ * The input starts process.
+ * The output is called by process when it is done.
+ * @class
+*/
+class Element {
+    /**
+     * Create an element.
+     * @constructor
+    */
+    constructor() {
+    }
+
+    /**
+     * @abstract
+     * @param {*} data - The input data
+     * @description After this will enter next process.
+    */
+    input(data) {
+    }
+
+    process() {
+    }
+}
+
+export default Element;

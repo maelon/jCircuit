705e846dfd0b0e236c3a852462a40cb072412761
maelon <maelon.j@gmail.com>
2017-10-01 10:34:37 +0800

    pudate

--- src/jcircuit.js
+++ src/jcircuit.js
@@ -1,5 +1,25 @@
+/*===================================================================
+#    FileName: jcircuit.js
+#      Author: Maelon.J
+#       Email: maelon.j@gmail.com
+#  CreateTime: 2017-09-02 11:13
+# Description: jCircuit主类
+===================================================================*/
+
 'use strict';
 
-const func = () => 'hello, world';
+/**
+* jCircuit is a simple javascript framework.
+* It works like circuit.
+*/
+class JCircuit {
 
-export default func;
+    /**
+    * @construcor
+    *
+    */
+    constructor() {
+    }
+}
+
+export default JCircuit;
